e9c3b0a2274dc52b012c54c7c177b030e3fa8a01
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2020-11-27 13:57:36 +0500

    Added frontend Integration

---
+++ step05_appsync_apollo_client/gatsby-frontend/src/wrap-root-element.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./apollo/client"
+
+export default ({ element }) => (
+  <ApolloProvider client={client}>
+        {element}
+  </ApolloProvider>
+)

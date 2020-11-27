93242b544d511c37a738d805fb1ab1939b5f4925
warishasan <waris.hasan00@gmail.com>
2020-11-27 16:45:13 +0500

    implemented front-end and back-end
    integrated appsync at client-side using aws amplify.

---
+++ step06_appsync_frontend_amplify/frontend/src/graphql/queries.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTodos = /* GraphQL */ `
+  query GetTodos {
+    getTodos {
+      title
+    }
+  }
+`;

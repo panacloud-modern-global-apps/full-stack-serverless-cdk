2b2aa454afc55e2ac6aa46179c828596cb0b3151
uzairbangee <uzairbangee@gmail.com>
2020-11-29 14:19:27 +0500

    fix: change in folder name

--- step03_appsync_lambda_as_datasource/lib/step03_appsync_lambda_as_datasource-stack.ts
+++ step03_appsync_lambda_as_datasource/lib/step03_appsync_lambda_as_datasource-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as appsync from '@aws-cdk/aws-appsync';
 import * as lambda from '@aws-cdk/aws-lambda';
 
-export class Step04AppsyncLambdaAsDatasourceStack extends cdk.Stack {
+export class Step03AppsyncLambdaAsDatasourceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 

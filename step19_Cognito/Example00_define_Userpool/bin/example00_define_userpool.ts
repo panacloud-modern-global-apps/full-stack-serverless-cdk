8f5b2a6060047aa57dde6999c3cd6aedf5002a11
uzairbangee <uzairbangee@gmail.com>
2020-12-05 14:56:49 +0500

    feat: move cognito to another branch

--- step19_Cognito/Example00_define_Userpool/bin/example00_define_userpool.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { Example00DefineUserpoolStack } from '../lib/example00_define_userpool-stack';
-
-const app = new cdk.App();
-new Example00DefineUserpoolStack(app, 'Example00DefineUserpoolStack');

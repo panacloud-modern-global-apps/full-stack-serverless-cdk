ef4ee44f114d7686bd42ecac4cbce621b4d7cc0a
Hamzah-syed <50793209+Hamzah-syed@users.noreply.github.com>
2020-12-11 23:53:18 +0500

    readme added of example00 x-ray step

--- stepxx_x-ray/example00_simple_lambda_tracing/lib/example00_simple_lambda_tracing-stack.ts
+++ stepxx_x-ray/example00_simple_lambda_tracing/lib/example00_simple_lambda_tracing-stack.ts
@@ -1,10 +1,10 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 
 export class Example00SimpleLambdaTracingStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new lambda.Function(this, "lambda-x-ray-Demo", {
+    new lambda.Function(this, "lambda-x-ray-tracing", {
       runtime: lambda.Runtime.NODEJS_10_X,
       code: lambda.Code.fromAsset("lambda"),
       handler: "app.handler",

2362467f3b0c7535b96a8951a81b94a678e52f47
Hamzah-syed <50793209+Hamzah-syed@users.noreply.github.com>
2020-12-12 18:36:36 +0500

    x-ray step01 readme added

--- stepxx_x-ray/step01_lambda_with_s3_tracing/lambda/app.ts
+++ stepxx_x-ray/step01_lambda_with_s3_tracing/lambda/app.ts
@@ -7,7 +7,7 @@ exports.handler = (event: APIGatewayEvent) => {
   //variable that uses the sdk to record any s3 bucket activity in this application
   const s3 = AWSXRay.captureAWSClient(new AWS.S3());
 
-  //this function list all the s3 buckets
+  //this function lists all the s3 buckets
   s3.listBuckets((err, data) => {
     if (data) {
       console.log("Success", data.Buckets);

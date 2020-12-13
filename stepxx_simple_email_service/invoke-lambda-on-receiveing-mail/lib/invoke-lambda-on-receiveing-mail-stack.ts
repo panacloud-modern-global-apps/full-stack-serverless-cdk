5ce5fcd73e9be4418f7c9cca2e2cb9a880b7f2f4
murtazahuzaifa <murtaza.huzaifa2@gmail.com>
2020-12-13 17:00:42 +0500

    readme file added in step_simple_email_service/invoke-lambda-on-receiving-mail

--- stepxx_simple_email_service/invoke-lambda-on-receiveing-mail/lib/invoke-lambda-on-receiveing-mail-stack.ts
+++ stepxx_simple_email_service/invoke-lambda-on-receiveing-mail/lib/invoke-lambda-on-receiveing-mail-stack.ts
@@ -24,24 +24,24 @@ export class InvokeLambdaOnReceiveingMailStack extends cdk.Stack {
     })
 
     // creating instance for taking email input while deployment
-    const emailAddress = new cdk.CfnParameter(this, 'emailParam',{
-      type: 'String', description:"Write your recipient email: "
+    const emailAddress = new cdk.CfnParameter(this, 'emailParam', {
+      type: 'String', description: "Write your recipient email"
     });
 
     // Adding a rule inside a rule set
     ruleSet.addRule('INVOKE_LAMBDA_RULE', {
       recipients: [emailAddress.valueAsString], // if no recipients than the action will be called on any incoming mail addresses of verified domains
-            actions: [
+      actions: [
         new actions.Lambda({ // defining an action to call when receive email on given recipients
           function: actionLambda,
           invocationType: actions.LambdaInvocationType.EVENT,
         }),
-      ], 
+      ],
       scanEnabled: true, // Enable spam and virus scanning
     })
 
 
-    
+
 
   }
 }

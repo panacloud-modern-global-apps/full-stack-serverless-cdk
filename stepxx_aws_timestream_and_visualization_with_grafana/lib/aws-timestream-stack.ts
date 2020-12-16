40b3a909924d1d3266aa24c543ff288864a515e0
abdulrafayghani <abdulrafay.ghani@gmail.com>
2020-12-16 23:53:57 +0500

    policy updated

--- stepxx_aws_timestream_and_visualization_with_grafana/lib/aws-timestream-stack.ts
+++ stepxx_aws_timestream_and_visualization_with_grafana/lib/aws-timestream-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as timestream from '@aws-cdk/aws-timestream';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as iam from '@aws-cdk/aws-iam';
 import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class AwsTimestreamStack extends cdk.Stack {
@@ -13,7 +14,7 @@ export class AwsTimestreamStack extends cdk.Stack {
     });
 
     const DBTable = new timestream.CfnTable(this, 'TSTable', {
-      tableName: "TSTable",
+      tableName: 'TSTable',
       databaseName: TimeStreamDB.databaseName!,
     });
 
@@ -23,16 +24,23 @@ export class AwsTimestreamStack extends cdk.Stack {
       handler: 'main.handler',
     });
 
-    TSlambda.addEnvironment("TS_DATABASE_NAME", DBTable.databaseName)
-    TSlambda.addEnvironment("TS_TABLE_NAME", DBTable.tableName!)
+    TSlambda.addEnvironment('TS_DATABASE_NAME', DBTable.databaseName);
+    TSlambda.addEnvironment('TS_TABLE_NAME', DBTable.tableName!);
 
-    const postStepsIntegration = new apigateway.LambdaIntegration(TSlambda)
+    const postStepsIntegration = new apigateway.LambdaIntegration(TSlambda);
 
-    const api = new apigateway.RestApi(this, "TSApi", {
-      restApiName: "TSApi",
-      description: "Testing TSDB"
-    })
+    const api = new apigateway.RestApi(this, 'TSApi', {
+      restApiName: 'TSApi',
+      description: 'Testing TSDB',
+    });
 
-    api.root.addMethod("POST", postStepsIntegration); 
+    api.root.addMethod('POST', postStepsIntegration);
+
+    const policy = new iam.PolicyStatement();
+    policy.addActions(
+      'timestream:DescribeEndpoints',
+      'timestream:WriteRecords'
+    );
+    policy.addResources('*');
   }
 }

92158e3cc1d55b9cac8d5a59fa91a53b1e3233a4
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2020-12-17 20:47:35 +0500

    lambda updated

--- stepxx_simple_queue_service/step1_sqs_to_lambda/lambda/index.js
+++ stepxx_simple_queue_service/step1_sqs_to_lambda/lambda/index.js
@@ -1,7 +1,7 @@
-export async function handler(event, context) {
+exports.handler = async (event, context) => {
   event.Records.forEach((record) => {
     const { body } = record;
     console.log(body);
   });
   return {};
-}
+};

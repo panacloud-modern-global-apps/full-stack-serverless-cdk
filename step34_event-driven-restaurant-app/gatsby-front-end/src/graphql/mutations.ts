ff98adaee1fe43d03b3474b12a2be686ab22835d
uzairbangee <uzairbangee@gmail.com>
2020-12-19 15:33:27 +0500

    feat: frontend feature added to event driven

--- step34_event-driven-restaurant-app/gatsby-front-end/src/graphql/mutations.ts
+++ step34_event-driven-restaurant-app/gatsby-front-end/src/graphql/mutations.ts
@@ -1,19 +1,44 @@
 
 // generateAction(timeSlotId: ID!, userName:String!, action:ACTIONS): ResponseOutput
-export const generateAction = `
-mutation  GenerateAction($action: ACTIONS!, $timeSlotId: String!, $userName:String! ){
-  generateAction(action:$action, timeSlotId:$timeSlotId, userName:$userName){
-    action
-    userName
-    timeSlotId
+export const generateAction = /* GraphQL */ `
+  mutation  GenerateAction($timeSlotId: ID!, $userName:String!, $action: ACTIONS){
+    generateAction(timeSlotId:$timeSlotId, userName:$userName, action:$action){
+      action
+      userName
+      timeSlotId
+    }
   }
-}
 `;
 
 // addBookingRequest(id: ID!, userName:String!): Event
-export const addBookingRequest = `
+export const addBookingRequest = /* GraphQL */ `
 mutation addBookingRequest($id: ID!, $userName:String!){
   addBookingRequest(id: $id, userName: $userName){
+    result
+  }
+}
+`
+
+
+export const bookTimeSlot = /* GraphQL */ `
+mutation bookTimeSlot($id: ID!){
+  bookTimeSlot(id: $id){
+    result
+  }
+}
+`
+
+export const deleteBookingRequest = /* GraphQL */ `
+mutation deleteBookingRequest($id: ID!){
+  deleteBookingRequest(id: $id){
+    result
+  }
+}
+`
+
+export const addTimeSlot = /* GraphQL */ `
+mutation addTimeSlot($timeSlot: InputTimeSlot!){
+  addTimeSlot(timeSlot: $timeSlot){
     result
   }
 }

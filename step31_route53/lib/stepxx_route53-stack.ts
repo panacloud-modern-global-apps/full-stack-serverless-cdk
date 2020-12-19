501b860bd4a70fb9b9d3a8463786c05d8b9343b3
hamzah <you@example.com>
2020-12-19 17:40:50 -0500

    step 31 code completed

--- step31_route53/lib/stepxx_route53-stack.ts
+++ step31_route53/lib/stepxx_route53-stack.ts
@@ -5,6 +5,9 @@ import * as s3 from "@aws-cdk/aws-s3";
 import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 import * as route53 from '@aws-cdk/aws-route53';
 import * as targets from '@aws-cdk/aws-route53-targets';
+import * as acm from "@aws-cdk/aws-certificatemanager"
+import { IHostedZone } from '@aws-cdk/aws-route53';
+import { ICertificate } from '@aws-cdk/aws-certificatemanager';
 
 
 export class StepxxRoute53Stack extends cdk.Stack {
@@ -16,44 +19,41 @@ const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
   versioned: true,
 });
 
-// create a CDN to deploy your website
 
-const distribution = new cloudfront.Distribution(this, "Distribution", {
-  defaultBehavior: {
-    origin: new origins.S3Origin(websiteBucket),
-
-    
-  },
-  defaultRootObject: "index.html",
-  enableIpv6:true,
-  
-
-  
-});
-
-
-// housekeeping for uploading the data in bucket 
-
-new s3deploy.BucketDeployment(this, "DeployWebsite", {
-  sources: [s3deploy.Source.asset("./website")],
-  destinationBucket: websiteBucket,
-  distribution,
-  distributionPaths: ["/*"],
-});
 
 //creating hosted zone for our domain
 const myZone= new route53.PublicHostedZone(this, 'HostedZone', {
   zoneName: 'panacloud.tk',
+}) as IHostedZone;
 
+//ssl ceritificate
+const certificate =new acm.DnsValidatedCertificate(this, 'CrossRegionCertificate', {
+  domainName: 'panacloud.tk',
+  hostedZone: myZone as any,
+  region: 'us-east-1',
+  
+})as ICertificate;
 
+// create a CDN to deploy your website
+const distribution = new cloudfront.Distribution(this, "Distribution", {
+  defaultBehavior: {
+    origin: new origins.S3Origin(websiteBucket),
+
+  },
+  defaultRootObject: "index.html",
+  enableIpv6:true,
+  domainNames:["panacloud.tk"],
+  certificate:certificate as any,
 });
 
-// Adding AAAA(ipv6) record
-new route53.AaaaRecord(this, 'Alias', {
-  zone: myZone,
-  target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+  // housekeeping for uploading the data in bucket 
 
-});
+  new s3deploy.BucketDeployment(this, "DeployWebsite", {
+    sources: [s3deploy.Source.asset("./frontend")],
+    destinationBucket: websiteBucket,
+    distribution,
+    distributionPaths: ["/*"],
+  });
 
 // Adding A(ipv4) record
 new route53.ARecord(this, 'AliasA', {
@@ -61,6 +61,14 @@ new route53.ARecord(this, 'AliasA', {
   target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
 
 });
+//Adding ipv6 record
+new route53.AaaaRecord(this, 'Alias', {
+  zone: myZone,
+  target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+});
+
+
+  
 
 
   }

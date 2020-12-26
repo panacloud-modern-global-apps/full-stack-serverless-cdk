3890872dd2dfa3667441108cf36845c8379e8c18
Translucent504 <fleshlime.1@gmail.com>
2020-12-26 18:31:50 +0500

    cleanup

--- step11_Cognito/Example03_google_login_cognito_amplify/cdk-backend-old/bin/example03_google_login_cognito.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { Example03GoogleLoginCognitoStack } from '../lib/example03_google_login_cognito-stack';
-
-const app = new cdk.App();
-new Example03GoogleLoginCognitoStack(app, 'Example03GoogleLoginCognitoStack');

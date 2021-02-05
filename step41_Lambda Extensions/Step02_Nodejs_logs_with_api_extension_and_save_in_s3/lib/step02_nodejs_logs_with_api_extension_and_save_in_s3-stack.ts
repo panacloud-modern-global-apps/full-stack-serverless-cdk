5ec354b675e7ce6b8c6a996bf824c26952cc1eb7
shamaz332 <shamazsaeed332@gmail.com>
2021-02-05 20:51:23 +0500

    step 41 added_

--- step41_Lambda Extensions/Step02_Nodejs_logs_with_api_extension_and_save_in_s3/lib/step02_nodejs_logs_with_api_extension_and_save_in_s3-stack.ts
+++ step41_Lambda Extensions/Step02_Nodejs_logs_with_api_extension_and_save_in_s3/lib/step02_nodejs_logs_with_api_extension_and_save_in_s3-stack.ts
@@ -1,4 +1,4 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as iam from "@aws-cdk/aws-iam";
@@ -9,24 +9,35 @@ export class Step02NodejsLogsWithApiExtensionAndSaveInS3Stack extends cdk.Stack 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, 'DemoBucket');
+    //========================================
+    // S3 bucket where logs will br saved
+    //========================================
 
+    const bucket = new s3.Bucket(this, "DemoBucket");
+    //========================================
+    // Lambda Layer that will be used as Extension
+    //========================================
     const lambdaLayer = new lambda.LayerVersion(this, "lambdalayer", {
       code: lambda.Code.fromAsset("lambda-layers"),
-
     });
 
-    const role = new iam.Role(this, 'Role', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    //========================================
+    // Permessions
+    //========================================
 
+    const role = new iam.Role(this, "Role", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
     });
     const policy = new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
-      actions: ["lambda:*", "s3:*"],
+      actions: ["s3:*"],
       resources: ["*"],
     });
     role.addToPolicy(policy);
-    
+
+    //========================================
+    // Lambda Func
+    //=======================================
 
     const sendfunc = new lambda.Function(this, "sendlog", {
       code: lambda.Code.fromAsset("lambda"),
@@ -34,15 +45,10 @@ export class Step02NodejsLogsWithApiExtensionAndSaveInS3Stack extends cdk.Stack 
       handler: "hello.handler",
       memorySize: 1024,
       layers: [lambdaLayer],
-      role:role,
+      role: role,
       environment: {
         S3_BUCKET_NAME: bucket.bucketName,
       },
-
     });
-
-
-    
-
   }
 }

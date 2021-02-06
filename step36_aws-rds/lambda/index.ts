977d21fb33c76a80668f8b1e93db1f600c34f7c2
zh <50803056+hhzule@users.noreply.github.com>
2021-02-06 11:34:35 +0500

    Update index.ts

--- step36_aws-rds/lambda/index.ts
+++ step36_aws-rds/lambda/index.ts
@@ -5,7 +5,7 @@ import {
 } from "aws-lambda";
 import { env } from "process";
 
-const value = env.vala || "ll";
+const value = env.vala || "none";
 const envvalue2: any = JSON.parse(value);
 
 // Require and initialize outside of your main handler
@@ -35,19 +35,6 @@ export async function handler(
       "CREATE TABLE IF NOT EXISTS new (task_id INT AUTO_INCREMENT, description TEXT, PRIMARY KEY (task_id))"
     );
 
-    // let resultsb = await mysql.query(
-    //   "insert into new (task_id, description) values(20,'important')"
-    // );
-
-    // Query with advanced options
-    // let resultsc= await connect.query({
-    //   sql: 'SELECT * FROM table WHERE name = ?',
-    //   timeout: 10000,
-    //   values: ['serverless']
-    // })
-
-    // let results = await mysql.query("SELECT * FROM new");
-
     console.log(resultsa, "results from database");
 
     //  close the connection
@@ -55,10 +42,8 @@ export async function handler(
 
     // first query
     // "CREATE TABLE IF NOT EXISTS new (task_id INT AUTO_INCREMENT, description TEXT, PRIMARY KEY (task_id))"
-
     // second query
     // "insert into new (task_id, description) values(20,'humddda')",
-
     // third query
     // "SELECT * FROM new"
 
@@ -76,13 +61,3 @@ export async function handler(
     };
   }
 }
-// ***************************************************************************************************************
-
-// Transaction support in serverless-mysql has been dramatically simplified. Start a new transaction using the transaction() method, and then chain queries using the query() method. The query() method supports all standard query options. Alternatively, you can specify a function as the only argument in a query() method call and return the arguments as an array of values. The function receives two arguments, the result of the last query executed and an array containing all the previous query results. This is useful if you need values from a previous query as part of your transaction.
-
-// You can specify an optional rollback() method in the chain. This will receive the error object, allowing you to add additional logging or perform some other action. Call the commit() method when you are ready to execute the queries.
-// let results = await mysql.transaction()
-//   .query('INSERT INTO table (x) VALUES(?)', [1])
-//   .query('UPDATE table SET x = 1')
-//   .rollback(e => { /* do something with the error */ }) // optional
-//   .commit() // execute the queries

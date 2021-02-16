c21ee0acce406f699e1ab827a9556f1d42a7cc7e
Roomi <nailaparwin@gmail.com>
2021-02-16 11:42:46 +0500

    Create aws-exports.js

---
+++ step43_kinesis/step00_kinesis_stream/gatsby-frontend/src/aws-exports.js
@@ -0,0 +1,7 @@
+const config = {
+    'aws_project_region': 'us-east-1',
+    'aws_cognito_identity_pool_id': 'us-east-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+    'aws_stream_name': 'my-first-stream'
+};
+
+export default config;

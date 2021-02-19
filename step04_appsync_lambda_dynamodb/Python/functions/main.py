cfe08c5f1827bb4bbe29cee6c487c570b19ef507
shamaz332 <shamazsaeed332@gmail.com>
2021-02-19 21:52:29 +0500

    lambda in python added

---
+++ step04_appsync_lambda_dynamodb/Python/functions/main.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+from addTodo import addTodoItem
+from getTodo import getItem
+from deleteTodo import deleteItem
+from updateTodo import updateItem
+import os
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+
+def handler(event, context):
+
+    field = event['info']['fieldName']
+
+    if field == "addTodo":
+        todo = event['arguments']['todo']
+        addTodoItem(todo)
+    elif field == "getTodos":
+        getItem()
+    elif field == "deleteTodo":
+        todoId = event['arguments']['todoId']
+        deleteItem(todoId)
+    elif field == "updateTodo":
+        todo = event['arguments']['todo']
+        updateItem(todo)
+    else:
+        print("No matched")

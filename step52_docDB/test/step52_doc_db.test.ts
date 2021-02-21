9ea82201c644c331837420999fd5eaa98f6a2248
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-02-22 03:02:26 +0500

    add python

--- step52_docDB/test/step52_doc_db.test.ts
+++ step52_docDB/test/step52_doc_db.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as Example01LambdaDestinationWithDifferentDestinations from '../lib/example01_lambda_destination_with_different_destinations-stack';
+import * as Step52DocDb from '../lib/step52_doc_db-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Example01LambdaDestinationWithDifferentDestinations.Example01LambdaDestinationWithDifferentDestinationsStack(app, 'MyTestStack');
+    const stack = new Step52DocDb.Step52DocDbStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}

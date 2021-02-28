2143d1ffb36b230925b7d6335beb819c4e5a5069
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-02-28 15:30:53 +0500

    update readme

--- step47_ec2/lib/step47_ec2-stack.ts
+++ step47_ec2/lib/step47_ec2-stack.ts
@@ -1,11 +1,18 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import { KeyPair } from 'cdk-ec2-key-pair';
 
 export class Step47Ec2Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // Create the Key Pair
+    const key = new KeyPair(this, 'A-Key-Pair', {
+      name: 'a-key-pair',
+      description: 'This is a Key Pair',
+      storePublicKey: true, // by default the public key will not be stored in Secrets Manager
+    });
 
     // Create new VPC
     const vpc = new ec2.Vpc(this, 'VPC', {
@@ -34,6 +41,7 @@ export class Step47Ec2Stack extends cdk.Stack {
     // Instance details
     //use instance class T2 and instance size micro coz it's in free tier 
     new ec2.Instance(this, 'Instance', {
+      keyName: key.keyPairName,
       instanceName: 'mkk-ec2-instance', //optional
       vpc,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
